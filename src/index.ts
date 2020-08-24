869ac74af777deeb1e4ca2f626cc9c9de1d87eb7
uruha <kochmusica@gmail.com>
2020-08-24 13:06:39 +0900

    update: any

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as ncu from 'npm-check-updates';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as ncu from "npm-check-updates";
 
 const exec = async () => {
   const pr = github.context.payload.pull_request;
   if (!pr) {
-    console.log('github.context.payload.pull_request not exist');
+    console.log("github.context.payload.pull_request not exist");
     return;
   }
 
-  const token = process.env['GITHUB_TOKEN'];
+  const token = process.env["GITHUB_TOKEN"];
   if (!token) {
-    console.log('GITHUB_TOKEN not exist');
+    console.log("GITHUB_TOKEN not exist");
     return;
   }
 
   const octokit = github.getOctokit(token);
-  const repoWithOwner = process.env['GITHUB_REPOSITORY'];
+  const repoWithOwner = process.env["GITHUB_REPOSITORY"];
   // @ts-ignore
-  const [owner, repo] = repoWithOwner.split('/');
+  const [owner, repo] = repoWithOwner.split("/");
 
   try {
-    const packageManager = core.getInput('package-manager');
+    const packageManager = core.getInput("package-manager");
     const upgraded = await ncu.run({
-      packageManager: `${packageManager ? packageManager : 'npm'}`
+      packageManager: `${packageManager ? packageManager : "npm"}`,
     });
     console.info(upgraded);
 
     let bodyMessage: string;
-    if(Object.keys(upgraded).length > 0) {
-      bodyMessage = `⚠️ Check dependencies to upgrade: ${JSON.stringify(upgraded)}`;
+    if (Object.keys(upgraded).length > 0) {
+      bodyMessage = `⚠️ Check dependencies to upgrade: ${
+        JSON.stringify(upgraded)
+      }`;
     } else {
-      bodyMessage = '👍 All the latest modules 🙆‍♀️';
+      bodyMessage = "👍 All the latest modules 🙆‍♀️";
     }
 
     const response = await octokit.issues.createComment({
@@ -42,7 +44,7 @@ const exec = async () => {
     });
     console.log(`created comment URL: ${response.data.html_url}`);
 
-    core.setOutput('commentUrl', response.data.html_url);
+    core.setOutput("commentUrl", response.data.html_url);
   } catch (error) {
     core.setFailed(error.message);
   }
